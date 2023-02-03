3ab02078e73fb8acace45f83191e888061bff8a1
johnmartinezc <john.martinez@codeimmersives.com>
2023-02-03 14:47:35 -0500

    -all

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//instantiate standard libraries
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+//setup router for each set of routes 
+// importing from routes/ folder 
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const blogsRouter = require('./routes/blogs');
+// const newBlog = require('./routes/blogs.js')
+//instantiate the actual express app
+const app = express();
 
 // view engine setup
+// sets application settings. (things we can access across the application)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//associating the libraries with the app
+// adding middleware 
+//(adding libraries that we can use throughout our application)
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+//for hosting static files: css, html, images etc. 
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+//we bind (associate) the routers to routes in our application
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/blogs', blogsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +50,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// app.listen(port, () => {
+//   console.log(`ExpressBlogger app listening on port ${port}`)
+// })
 
 module.exports = app;
